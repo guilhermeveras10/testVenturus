e3c92e64caf55e100dc0871d5174721b827b6d1e
guilherme <guilhermeveras10@hotmail.com>
2020-06-11 13:50:31 -0300

    add  comments

--- TestVenturus/Service/APIManager.swift
+++ TestVenturus/Service/APIManager.swift
@@ -10,10 +10,13 @@ import Foundation
 import Alamofire
 
 struct URLTest {
+    /*
+     A API passada estava com instabilidades hora respondia hora não, optei por copiar a response e colocar numa URL estática, por isso o endpoint está diferente.
+     */
 //    static var url = "https://api.imgur.com/3/gallery/search/?q=cats"
     static var url = "https://demo1255337.mockable.io/cats"
 //    static var private_key = "63775118a9f912fd91ed99574becf3b375d9eeca"
-    static var public_key = "Client-ID 1ceddedc03a5d71"
+//    static var public_key = "Client-ID 1ceddedc03a5d71"
     
 }
 struct DataService {
@@ -26,10 +29,10 @@ struct DataService {
     
     func getInfo(completion: @escaping (ImgUrlInfo?) -> Void) {
         
-        let headers: HTTPHeaders = [
-            "Authorization": URLTest.public_key,
-            "Content-type": "application/json"
-        ]
+//        let headers: HTTPHeaders = [
+//            "Authorization": URLTest.public_key,
+//            "Content-type": "application/json"
+//        ]
         
         Alamofire.request(URLTest.url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
             
